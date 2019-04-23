21f53bd184ffc0926976e950cecf2afaed081954
Yaroslav Paramonov <student.help.96@gmail.com>
2019-04-23 12:34:09 +0300

    fix common dao-all tests passed

--- src/main/java/com/cybernation/testservice/repositories/DemoMongoModelDao.java
+++ src/main/java/com/cybernation/testservice/repositories/DemoMongoModelDao.java
@@ -1,12 +1,12 @@
 package com.cybernation.testservice.repositories;
 
 import com.cybernation.testservice.models.DemoMongoModel;
-import com.extremum.common.dao.MongoCommonDao;
+import com.extremum.common.dao.MorphiaMongoCommonDao;
 import org.mongodb.morphia.Datastore;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class DemoMongoModelDao extends MongoCommonDao<DemoMongoModel> {
+public class DemoMongoModelDao extends MorphiaMongoCommonDao<DemoMongoModel> {
     public DemoMongoModelDao(Datastore datastore) {
         super(datastore);
     }
