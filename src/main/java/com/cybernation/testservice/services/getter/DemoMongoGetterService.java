58080262ecceec41d97045e008a46984d338d10a
Yaroslav Paramonov <student.help.96@gmail.com>
2019-05-14 20:50:19 +0300

    unimplement getter service for demo mongo model

--- src/main/java/com/cybernation/testservice/services/getter/DemoMongoGetterService.java
+++ src/main/java/com/cybernation/testservice/services/getter/DemoMongoGetterService.java
@@ -2,23 +2,15 @@ package com.cybernation.testservice.services.getter;
 
 import com.cybernation.testservice.models.DemoMongoModel;
 import com.cybernation.testservice.services.DemoMongoService;
-import com.extremum.everything.services.GetterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class DemoMongoGetterService implements GetterService<DemoMongoModel> {
+public class DemoMongoGetterService {
     private final DemoMongoService demoMongoService;
 
-    @Override
     public DemoMongoModel get(String id) {
         return demoMongoService.get(id);
-    }
-
-    @Override
-    public String getSupportedModel() {
-        return "";
-//        return DemoMongoModel.MODEL_NAME;
     }
 }
