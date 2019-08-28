f89135d9b4476fe81b6a6f75177104bb7adf355c
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2019-08-28 18:35:23 +0400

    CS-539 Merge CollectionDescriptor to Descriptor
    - remove an unused method

--- src/test/java/com/cybernation/testservice/MockedMapperDependencies.java
+++ src/test/java/com/cybernation/testservice/MockedMapperDependencies.java
@@ -1,24 +1,14 @@
 package com.cybernation.testservice;
 
-import io.extremum.common.collection.service.CollectionDescriptorService;
 import io.extremum.common.descriptor.factory.DescriptorFactory;
 import io.extremum.common.mapper.MapperDependencies;
-
-import static org.mockito.Mockito.mock;
 
 /**
  * @author rpuch
  */
 public class MockedMapperDependencies implements MapperDependencies {
-    private final CollectionDescriptorService collectionDescriptorService = mock(CollectionDescriptorService.class);
-
     @Override
     public DescriptorFactory descriptorFactory() {
         return new DescriptorFactory();
-    }
-
-    @Override
-    public CollectionDescriptorService collectionDescriptorService() {
-        return collectionDescriptorService;
     }
 }
