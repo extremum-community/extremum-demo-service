34d608f61afa512f92ca2ee8cb76bd24fd446ab2
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2020-02-05 16:54:29 +0400

    CS-1191 Upgrade extremum-common to Spring Boot 2.2.x
    - migrate to new dependencies

--- src/main/java/com/cybernation/testservice/config/SecurityConfiguration.java
+++ src/main/java/com/cybernation/testservice/config/SecurityConfiguration.java
@@ -5,8 +5,12 @@ import com.cybernation.testservice.services.auth.UserService;
 import io.extremum.authentication.api.AuthenticationException;
 import io.extremum.authentication.common.services.ApiKeyVerifier;
 import io.extremum.authentication.common.services.ExternalCredentialsAuthenticator;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.mgt.SecurityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author rpuch
@@ -21,6 +25,29 @@ public class SecurityConfiguration {
     @Bean
     public ApiKeyVerifier apiKeyVerifier() {
         return new OnlyTestApiKeyAllowed();
+    }
+
+    @Bean
+    public Object shiroSecurityManagerGlobalizer(SecurityManager securityManager) {
+        // FIXME: Here is the problem. Shiro relies on ThreadLocals. Most of this demo
+        // application works in a 'predictable same thread' way, with one exception:
+        // collection streaming. When collections are streamed, any shiro-related action
+        // fails. This seems to only be an issue when Spring's FrameworkServlet publishes
+        // 'request processed' event as it obtains the current principal there. Moreover,
+        // if anything was written to the stream (that is, if at least one collection element
+        // was returned), the response is already committed and the shiro-related error
+        // does not disturb request processing. But if there is nothing (the collection is empty
+        // or does not exist), the shiro-generated exception is caught and converted to 500.
+        // This is quite useless, so we initialize a global shiro security manager to avoid
+        // this exception.
+        // But actually this is a hack. We need to invent something, probably completely drop
+        // thread-locals usage (which probably means dropping Shiro).
+        return new Object() {
+            @PostConstruct
+            public void globalize() {
+                SecurityUtils.setSecurityManager(securityManager);
+            }
+        };
     }
 
     private static class OnlyTestApiKeyAllowed implements ApiKeyVerifier {
