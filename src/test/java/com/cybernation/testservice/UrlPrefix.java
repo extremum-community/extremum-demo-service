08baabba59dbf92f14239d5688f8282810599ca0
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2020-04-07 16:51:59 +0400

    CS-1316 Remove /v1 prefix from extremum controllers

--- src/test/java/com/cybernation/testservice/UrlPrefix.java
+++ src/test/java/com/cybernation/testservice/UrlPrefix.java
@@ -1,7 +1,15 @@
 package com.cybernation.testservice;
 
+/**
+ * May be used to add a prefix to all 'our' URLs centrally if
+ * the app specific prefix changes.
+ *
+ * @author rpuch
+ */
 public class UrlPrefix {
     public static String prefix(String url) {
-        return "/v1" + url;
+        return url;
     }
+
+    private UrlPrefix() {}
 }
